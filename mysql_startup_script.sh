6494df67f6dc49f61ffcf562859e5de5ce277219
kiranbanna <k.anna@f5.com>
2017-09-07 11:23:24 -0700

    Update mysql_startup_script.sh

--- mysql_startup_script.sh
+++ mysql_startup_script.sh
@@ -21,6 +21,7 @@
 ## Version #     Name       #                    NOTES                   ##
 ###########################################################################
 ## 11/23/15#  Thomas Stanley#    Created base functionality              ##
+## 09/07/17#  Kiran Anna#        Modified the input parameters           ##
 ###########################################################################
 
 apt-get update
@@ -28,8 +29,11 @@ apt-get update
 apt-get -y install build-essential libssl-dev binutils binutils-dev openssl
 apt-get -y install libdb-dev libexpat1-dev automake checkinstall unzip elinks
 
-echo mysql-server mysql-server/root_password password $1 | debconf-set-selections  
-echo mysql-server mysql-server/root_password_again password $1 | debconf-set-selections 
+name=$1
+key=$2
+
+echo mysql-server mysql-server/root_password password $key | debconf-set-selections  
+echo mysql-server mysql-server/root_password_again password $key | debconf-set-selections 
 
 apt-get -y install mysql-server
 apt-get -y install libmysqld-dev libdb-dev
@@ -37,5 +41,5 @@ apt-get -y install libmysqld-dev libdb-dev
 ipaddr=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 sed 's%127.0.0.1%'$ipaddr'%' -i /etc/mysql/my.cnf 
 service mysql restart
-mysql -u root -p${1} -Bse "CREATE DATABASE opencart;CREATE USER 'opencart'@'%' IDENTIFIED BY '"$1"';GRANT ALL ON opencart.* TO 'opencart'@'%';flush privileges;"
+mysql -u root -p${key} -Bse "CREATE DATABASE opencart;CREATE USER '"$name"'@'%' IDENTIFIED BY '"$key"';GRANT ALL ON opencart.* TO '"$name"'@'%';flush privileges;"
 echo "This MySQL Server is now ready to host OpenCart!"
